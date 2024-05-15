135ac25480fc5407c7bb3e29f1365ecb09706a56
Agfi <thoriq.aghfi60@gmail.com>
2024-05-15 09:04:46 +0700

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Agfi on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
